11c462099d9a9e5af182b2b7684efd15c1519c60
lbw1320028474 <1320028474@qq.com>
2017-08-03 18:30:18 +0800

    单本小说阅读器项目上传

---
+++ NovelRead/src/com/example/novelread/ReadPageActivity.java
@@ -0,0 +1,13 @@
+package com.example.novelread;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReadPageActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.readpage_activity_layout);
+	}
+}
